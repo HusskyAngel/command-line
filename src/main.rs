1a6a05937666d7cac461a39636ceb2e9c2a3ad56
husskyAngel <angel.juan@correounivalle.edu.co>
2023-09-07 17:30:06 -0500

    first steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    //getting the arguments
+    let args: Vec<String> =env::args().collect();
+
+    let query=&args[1];
+    let file_path=&args[2]; 
+
+    println!("searching for {}",query);
+    println!("in file {}",file_path);
+
+
+    let contents=fs::read_to_string(file_path)
+        .expect("should have been able to read the file");
+    println!("with text:\n{contents}")
+    
+
 }
