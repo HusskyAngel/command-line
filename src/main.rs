1c923006929a454a2f6b2b98710c7b5d272ead1f
Juan José Angel <angel.juan@correounivalle.edu.co>
2023-09-10 23:46:38 -0500

    factor

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-use std::{env, fs};
+use std::{env,process};
+use commandline::{Config,run};
 
 fn main() {
     //getting the arguments
     let args: Vec<String> =env::args().collect();
 
-    let query=&args[1];
-    let file_path=&args[2]; 
+    let config=Config::build(&args).unwrap_or_else(|err|{
+        println!("problem parsing arguments:{err}");
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("searching for {}",query);
-    println!("in file {}",file_path);
-
-
-    let contents=fs::read_to_string(file_path)
-        .expect("should have been able to read the file");
-    println!("with text:\n{contents}")
-    
-
+    if let Err(e)=run(config){
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
+
+
